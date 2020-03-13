03fae91f8afb74cd68cb5db64df44f4f4ce6e760
CSharp-Coach <csharpsoftwarecoach@gmail.com>
2020-03-13 15:50:54 +0100

    WPF und MVVM

---
+++ WPF/WPF-MVVM-01/ApplicationMvvm/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Shared.Interfaces;
+using Shared.Ioc;
+using System.Windows;
+using ViewModels;
+using Views;
+
+namespace ApplicationProject
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ConfigureServices(InversionOfControlContainer.Instance);
+
+            Application.Current.Resources.Add(InversionOfControlKey.IocWpfContainer, InversionOfControlContainer.Instance);
+
+            MainWindowView mainWindowView = new MainWindowView();
+            mainWindowView.Show();
+        }
+
+        private static void ConfigureServices(InversionOfControlContainer iocContainer)
+        {
+            iocContainer.RegisterViewModel<IViewModel, MainWindowViewModel>();
+        }
+    }
+}

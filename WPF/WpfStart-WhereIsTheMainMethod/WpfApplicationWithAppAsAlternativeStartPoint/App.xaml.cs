8308899389a4f9d2c66ed1efdb597e6f4fc5e9c5
CSharp-Coach <csharpsoftwarecoach@gmail.com>
2020-03-27 16:23:56 +0100

    Implementierung

---
+++ WPF/WpfStart-WhereIsTheMainMethod/WpfApplicationWithAppAsAlternativeStartPoint/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace WpfApplicationWithAppAsAlternativeStartPoint
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Hier ist es auch möglich, das MainWindow zu instanziieren, die Anwendung funktioniert auch.
+            // Aber an dieser Stelle haben wir keine Möglichkeit Anwendungsargumente abzufragen, die evtl. beim Start der Anwendung angegeben worden sind.
+            //var mainWindow = new MainWindow();
+            //mainWindow.Show();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Das MainWindow sollte in der OnStartup-Methode aufgerufen werden, denn hier haben wir die Möglichkeit die Anwendungsargumente abzufragen,
+            // die beim Start der Anwendung gemacht worden sind (siehe Parameter von OnStartup: StartupEventArgs e).
+            //
+            // Die Anwendungsargumente können zu Testzwecken unter folgendem Punkt gefunden und geändert werden:
+            //  -> Rechte Maustaste auf das Projekt klicken und im Kontextmenü findet man die Eigenschaften: unter Debuggen / Anwendungsargumente (hier lassen sich mit Leerzeichen getrennt Parameter angeben) 
+
+            var mode = e.Args[0];
+            var user = e.Args[1];
+            var password = e.Args[2];
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ WPF/WpfStart-WhereIsTheMainMethod/WpfApplicationWithMain/Programm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApplicationWithMain
+{
+    public class Programm
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // 1.) Der saubere Weg für eine einwandfrei funktionierende WPF-Anwendung.
+            var app = new Application();
+            app.Run(new MainWindow());
+
+            // 2.) Das würde nicht funktionieren: Das Fenster schließt sich sofort.
+            //var mainWindow = new MainWindow();
+            //mainWindow.Show();
+
+            // 3.) Das würde zwar funktionieren und das MainWindow zeigt sich an (über ShowDialog handelt es sich um ein modales Fenster).
+            //     - Aber globale Styles, Templates (die Applikationsweit) können ohne das Application-Objekt nicht definiert und zugewiesen werden.
+            //     - Über den Namespace Application.Current.... lassen sich applikationsweit Informationen abfragen, allerdings nur mit einem instanzierten Applications-Objekt.
+            //var mainWindow = new MainWindow();
+            //mainWindow.ShowDialog();
+        }
+    }
+}
